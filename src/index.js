57e5d485c0957a81942100b61bdd133c8cba96f9
Bailey Kane <bailey.orion.kane@gmail.com>
2018-07-23 12:40:58 -0500

    Strange versioning issue

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import LessonTabs from './LessonTabs'
-import TopicPills from './TopicPills2'
-import CourseList from './containers/courses/CourseList'
+import TopicPills from './TopicPills'
+import CourseList from './containers/CourseList'
 import ES6 from './containers/es6/es6'
-import CourseEditor from './containers/courses/CourseEditor'
-
 
 const ModuleListItemStateless = ({title}) =>
   <li className="list-group-item">
@@ -171,7 +169,7 @@ class App extends React.Component {
           <Route path='/hello' component={HelloWorld}/>
           <Route path='/pageParam/:something' component={PageParam}/>
           <Route path='/es6' component={ES6}/>
-          <Route path='/course/:courseId' component={CourseEditor}/>
+
         </div>
       </Router>
     );
