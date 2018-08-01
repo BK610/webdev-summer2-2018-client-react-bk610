916fe1bf0f57d2878e056f87d00acc6b4ec3cd47
BK610 <bailey.orion.kane@gmail.com>
2018-07-31 23:25:05 -0400

    Added code from prof. github

---
+++ src/containers/widgets/WidgetListContainer.js
@@ -0,0 +1,56 @@
+import {connect} from 'react-redux'
+import WidgetListComponent from './WidgetListComponent'
+
+const stateToPropertyMapper = state => (
+    {
+        widgets: state.widgets
+    }
+)
+
+const dispatcherToPropertyMapper = dispatch => (
+    {
+        deleteWidget: wid => dispatch({
+            type: 'DELETE_WIDGET',
+            widgetId: wid
+        }),
+        createWidget: w => dispatch({
+            type: 'CREATE_WIDGET',
+            widget: w
+        }),
+        updateWidget: w => dispatch({
+            type: 'UPDATE_WIDGET',
+            widget: w
+        }),
+        saveWidgets: () => dispatch({
+            type: 'SAVE_WIDGETS'
+        }),
+        loadAllWidgets: () => {
+            fetch('http://localhost:8080/api/widget')
+                .then(response => response.json())
+                .then(widgets => dispatch({
+                    type: 'FIND_ALL_WIDGETS',
+                    widgets: widgets
+                }))
+        },
+        up: (widgetId) => {
+            dispatch({
+                type: 'UP',
+                widgetId: widgetId
+            })
+        },
+        down: (widgetId) => {
+            dispatch({
+                type: 'DOWN',
+                widgetId: widgetId
+            })
+        }
+    }
+)
+
+const WidgetListContainer =
+    connect(
+        stateToPropertyMapper,
+        dispatcherToPropertyMapper)
+    (WidgetListComponent)
+
+export default WidgetListContainer
