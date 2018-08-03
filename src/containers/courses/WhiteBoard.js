fcae93c4b49987bf7712f0269f581b614b260976
BK610 <bailey.orion.kane@gmail.com>
2018-08-02 21:44:11 -0400

    Created ModuleRow.js

---
+++ src/containers/courses/WhiteBoard.js
@@ -0,0 +1,52 @@
+import React from  'react'
+
+import CourseEditor from './CourseEditor';
+import CourseList from './CourseList';
+import CourseRow from "./CourseRow";
+import CourseService from "../../services/CourseService";
+
+export default class CourseManager extends React.Component {
+    constructor() {
+        super();
+        this.courseService = CourseService.instance;
+        this.state = {
+            newCourse: {},
+            courses: []
+        };
+    }
+
+    componentDidMount() {
+        this.courseService.findAllCourses()
+            .then(courses => {
+                this.setState({courses: courses});
+            });
+    }
+
+    formChanged = (event) => {
+        this.setState({newCourse: {
+                title: event.target.value
+            }})
+    };
+
+    deleteCourse = (courseId) => {
+        this.courseService.deleteCourse(courseId)
+            .then(() => this.courseService.findAllCourses())
+            .then(courses => this.setState({courses: courses}))
+    };
+
+    createCourse = () => {
+        this.courseService.createCourse(this.state.newCourse)
+            .then(course  => this.courseService.findAllCourses())
+            .then(courses => this.setState({courses: courses}))
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Course Manager</h2>
+                <CourseList/>
+                {/*<CourseEditor/>*/}
+            </div>
+        )
+    }
+}
